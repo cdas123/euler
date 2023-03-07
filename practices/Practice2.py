78033c65587d6483369da2c11c077732e52c4595
cdas <chandrika.das@raisin.com>
2023-03-07 16:00:27 +0100

    renamed function names

--- practices/Practice2.py
+++ practices/Practice2.py
@@ -1,17 +1,17 @@
 # A python3 program to calculate sum of the even-valued fibonacci terms not exceeding four million
-import sys;
+import sys
 
 sys.set_int_max_str_digits(0)
 
 
-def sumOfEvenValuedFibonacciTerms(list_of_fibonacci_numbers):
+def sum_of_even_valued_fibonacci_terms(list_of_fibonacci_numbers):
     sum_of_even_numbers = sum(list(filter(lambda x: x % 2 == 0, list_of_fibonacci_numbers)))
     print(sum_of_even_numbers)
 
 
 
 class Solver:
-    def fibonacciSeries(self, n):
+    def fibonacci_series(self, n):
 
         if (n > 4000000):
             sys.exit("Cancelling the calculation as the number exceeds four million")
@@ -19,10 +19,10 @@ class Solver:
         for i in range(2, n):
             list_of_fibonacci_numbers.append(list_of_fibonacci_numbers[i - 1] + list_of_fibonacci_numbers[i - 2])
             i = i + 1
-        sumOfEvenValuedFibonacciTerms(list_of_fibonacci_numbers);
+        sum_of_even_valued_fibonacci_terms(list_of_fibonacci_numbers);
 
 
 if __name__ == '__main__':
     number = int(input("enter number\n"))
-    solver = Solver();
-    solver.fibonacciSeries(number);
+    solver = Solver()
+    solver.fibonacci_series(number)
